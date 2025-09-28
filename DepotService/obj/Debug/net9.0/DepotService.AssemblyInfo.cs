76f620ef1ed8b59ff1566c8d68bd75c91e3f2995
Randy3227 <randyrobinson3227@gmail.com>
2025-09-28 22:48:28 +0300

    f

--- DepotService/obj/Debug/net9.0/DepotService.AssemblyInfo.cs
+++ DepotService/obj/Debug/net9.0/DepotService.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DepotService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< Updated upstream
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e02495eabe64294cbe57339cbc67cbd311b8db2")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9611d445f26102e2f90a19925a3554b09a4b04e8")]
->>>>>>> Stashed changes
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+661742c2dc25e35870cff6135a9853e59d595efe")]
 [assembly: System.Reflection.AssemblyProductAttribute("DepotService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DepotService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
